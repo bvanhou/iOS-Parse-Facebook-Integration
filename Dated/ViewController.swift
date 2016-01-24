fa06054dedbd855d457ad6b5463d8a2e7c0dd346
Ben Van Houten <ben@benjamins-mbp.carolina.rr.com>
2016-01-24 13:14:27 -0500

    Initial Commit

---
+++ Dated/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dated
+//
+//  Created by Benjamin Van Houten on 1/24/16.
+//  Copyright © 2016 Benjamin Van Houten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
